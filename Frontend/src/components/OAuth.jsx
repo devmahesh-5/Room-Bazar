8e48eef10fc49154eb04fb47dd14e62522636281
Mahesh Bhandari <n31mahesh@gmail.com>
2025-06-20 21:47:54 +0545

    Package installation on frontend

--- Frontend/src/components/OAuth.jsx
+++ Frontend/src/components/OAuth.jsx
@@ -1,5 +1,5 @@
 // pages/OAuthCallback.jsx
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import authService from '../services/auth.services.js';
