b525af680a48a5c764b8f5429ff018446e50e00d
Mahesh Bhandari <n31mahesh@gmail.com>
2025-06-21 14:55:43 +0545

    no credential in khalti success

--- Frontend/src/services/payment.services.js
+++ Frontend/src/services/payment.services.js
@@ -18,9 +18,7 @@ class paymentServices {
 
     async afterPaymentKhalti(data){
         try {
-            const response = await axios.get(`${API}/api/v1/payments/khalti/success/${data.paymentId}?pidx=${data.pidx}`,{
-                withCredentials: true
-            });
+            const response = await axios.get(`${API}/api/v1/payments/khalti/success/${data.paymentId}?pidx=${data.pidx}`);
             if (!response) {
                 throw new Error("Error creating payment");
             }
